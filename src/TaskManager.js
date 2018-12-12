86d4009806d3fcb712a34eda41f0e7eae9e3e7d0
Jay <jwinstead@launchdarkly.com>
2018-12-11 18:51:31 -0800

    changes...

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -9,7 +9,7 @@ module.exports = class TaskManager {
     this._getTaskList(taskName).push(taskData)
   }
 
-  consume(taskName) {
+  await consume(taskName) {
     const taskData = await this._getTaskList(taskName).consume()
     return taskData
   }
