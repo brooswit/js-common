cb8b0def4ec25eda0af4b420018989ae2bb41233
Jay <jwinstead@launchdarkly.com>
2018-12-17 22:17:39 -0800

    fix

--- src/applyOpts.js
+++ src/applyOpts.js
@@ -1,6 +1,6 @@
 const argsFromOpts = require('./argsFromOpts')
 
-module.exports = function applyOpts (func, opts, context) {s
+module.exports = function applyOpts (func, opts, context) {
   let args = argsFromOpts(opts, func)
   return func.apply(context, args)
 }
