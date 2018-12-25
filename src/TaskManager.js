738def606e2357bb3dda0f16699fecb0137ce14e
Jay <jwinstead@launchdarkly.com>
2018-12-24 20:54:08 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -34,6 +34,7 @@ module.exports = class TaskManager {
   consume(taskName, taskHandler, taskContext) {
     let {payload, callback, context} = await this._getTaskList(taskName).shift()
     let taskResult = taskHandler.call(taskContext, payload)
+    callback.call(context, taskResult)
   }
 
   subscribe(taskName, subscriptionHandler, context) {
