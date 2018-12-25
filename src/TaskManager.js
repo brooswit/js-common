72abe4fceb4892a983b17cf4e24211a425ae3836
Jay <jwinstead@launchdarkly.com>
2018-12-24 20:56:36 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -14,7 +14,7 @@ module.exports = class TaskManager {
   }
 
   feed(taskName, payload) {
-    this._getTaskList(taskName).push(payload)
+    this._getTaskList(taskName).push({payload})
   }
 
   request(taskName, payload, responseHandler, responseContext) {
