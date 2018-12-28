356ff3c3a882727d688d55164f6cdc8654baeb6a
brooswit <brooswit@OldBoi.local>
2018-12-28 09:00:21 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/PromiseToEmit.js
+++ src/PromiseToEmit.js
@@ -1,32 +1,32 @@
 module.exports = function promiseToEmit(emitter, eventName, errorEventName, label) {
+    return new Promise((resolve, reject) => {
     console.debug('PROMISE TO EMIT ' + eventName + " for " + label)
-    return new Promise()
+    emitter.on(eventName, resolver)
+        if (errorEventName) {
+            emitter.on(errorEventName, rejecter)
+        }
+      
+        function resolver(payload) {
+            cleanup()
+            resolve(payload)
+        }
+
+        function rejecter(error) {
+            cleanup()
+            reject(error)
+        }
+
+        function cleanup() {
+            emitter.off(eventname, resolver)
+            if (errorEventName) {
+                emitter.off(errorEventName, rejecter)
+            }
+        }
+    })
 }
 class PromiseToEmit extends Promise {
   constructor(emitter, eventName, errorEventName, label) {
 
-      super((resolve, reject) => {
-            emitter.on(eventName, resolver)
-            if (errorEventName) {
-                emitter.on(errorEventName, rejecter)
-            }
-          
-            function resolver(payload) {
-                cleanup()
-                resolve(payload)
-            }
-
-            function rejecter(error) {
-                cleanup()
-                reject(error)
-            }
-
-            function cleanup() {
-                emitter.off(eventname, resolver)
-                if (errorEventName) {
-                    emitter.off(errorEventName, rejecter)
-                }
-            }
-        })
+      super()
     }
 }
