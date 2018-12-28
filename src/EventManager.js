679612889c9fe745fee07cf54f8eff3c7c3e2821
brooswit <brooswit@OldBoi.local>
2018-12-28 09:01:16 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/EventManager.js
+++ src/EventManager.js
@@ -1,6 +1,6 @@
 const EventEmitter = require('events')
 const Process = require('./Process')
-const PromiseToEmit = require('./PromiseToEmit')
+const promiseToEmit = require('./PromiseToEmit')
 
 module.exports = class EventManager {
   constructor() {
@@ -19,7 +19,7 @@ module.exports = class EventManager {
     return new Process(async (process) => {
       this._eventEmitter.on(eventName, eventHandler, eventContext)
       console.debug(`setting up ${eventName}...`)
-      let promise = new PromiseToEmit(process, 'close', null, 'EventManager.hook')
+      let promise = promiseToEmit(process, 'close', null, 'EventManager.hook')
       // console.debug(`ready ${eventName}...`)
       // await promise
       // console.debug(`teardown ${eventName}...`)
