c337720e7ce1106e7d8daadd53a8e4160939dfa1
brooswit <brooswit@OldBoi.local>
2018-12-28 08:58:51 -0800

    AUTOMATED DEVELOPMENT DEPLOYMENT

--- src/PromiseToEmit.js
+++ src/PromiseToEmit.js
@@ -1,5 +1,6 @@
 module.exports = class PromiseToEmit extends Promise {
   constructor(emitter, eventName, errorEventName, label) {
+      
       console.debug('PROMISE TO EMIT ' + eventName + " for " + label)
       super((resolve, reject) => {
             emitter.on(eventName, resolver)
