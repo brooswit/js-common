b46d5315f4e4365257602084c26ddc8883ec573d
Jay <jwinstead@launchdarkly.com>
2019-01-08 09:43:28 -0800

    squash! Automated Development Deployment

--- src/PromiseToEmit.js
+++ src/PromiseToEmit.js
@@ -1,20 +1,23 @@
 module.exports = function promiseToEmit(emitter, eventName, errorEventName, label) {
     console.log({emitter, eventName, errorEventName, label})
     return new Promise((resolve, reject) => {
-        const cleanup = () => {
+        const resolver, rejecter, cleanup
+
+        resolver = (payload) => {
+            cleanup()
+            resolve(payload)
+        }
+
+        rejecter = (error) => {
+            cleanup()
+            reject(error)
+        }
+        
+        cleanup = () => {
             emitter.off(eventname, resolver)
             if (errorEventName) {
                 emitter.off(errorEventName, rejecter)
             }
-        }
-
-        const resolver = (payload) => {
-            cleanup()
-            resolve(payload)
-        }
-        const rejecter = (error) => {
-            cleanup()
-            reject(error)
         }
 
         emitter.on(eventName, resolver)
