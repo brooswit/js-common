9f62ea9d2234075ef9e1935d772e92a988fffd36
Jay <jwinstead@launchdarkly.com>
2019-01-09 18:07:29 -0800

    squash! Automated Development Deployment

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -6,6 +6,11 @@ module.exports = class EventEmitter extends Process {
     }
     on(eventName, callback, scope) {
         this._contexts[eventName] = this._contexts[eventName] || []
+        this._contexts[eventName].push({callback, scope})
+    }
+    once(eventName, callback, scope) {
+        this._contexts[eventName] = this._contexts[eventName] || []
+        this._contexts[eventName].push({callback, scope, once: true})
     }
     off(eventName, callback, scope)
     emit(eventName, payload)
