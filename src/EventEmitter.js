a5f778e75240a56cd005992ed0577cec89535259
Jay <jwinstead@launchdarkly.com>
2019-01-09 18:02:02 -0800

    squash! Automated Development Deployment

---
+++ src/EventEmitter.js
@@ -0,0 +1,13 @@
+module.exports = class EventEmitter extends Process {
+    constructor() {
+        super(()=>{
+            
+        })
+    }
+    on(eventName, callback, context) {
+
+    }
+    off(eventName, callback, context)
+    emit(eventName, payload)
+
+}
