0990441a9e50267462080260f85bcf2466355cd6
Jay <jwinstead@launchdarkly.com>
2019-01-09 19:36:10 -0800

    squash! Automated Development Deployment

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -18,7 +18,7 @@ class EventListener extends Process {
         })
     }
 
-    emit(payload) {
+    async emit(payload) {
         if(this._once) this.close()
         return await (this.callback.call(this.scope, payload))
     }
