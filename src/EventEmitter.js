0cae2e614d2c9cb3691be525efdc5d3364ae1fc1
Jay <jwinstead@launchdarkly.com>
2019-01-09 18:10:49 -0800

    squash! Automated Development Deployment

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -1,18 +1,29 @@
 module.exports = class EventEmitter extends Process {
     constructor() {
+        this._nextRefId = 0
         super(()=>{
             await this.promiseToClose
         })
     }
     on(eventName, callback, scope) {
+        let refId = this._nextRefId ++
         this._contexts[eventName] = this._contexts[eventName] || []
-        this._contexts[eventName].push({callback, scope})
+        this._contexts[eventName].push({refId, callback, scope})
     }
     once(eventName, callback, scope) {
+        let refId = this._nextRefId ++
         this._contexts[eventName] = this._contexts[eventName] || []
-        this._contexts[eventName].push({callback, scope, once: true})
+        this._contexts[eventName].push({refId, callback, scope, once: true})
     }
-    off(eventName, callback, scope)
-    emit(eventName, payload)
+    off(eventName, callbackOrRefId, scope) {
+        let callback = refId = callbackOrRefId
+        for(let contextIndex in this._contexts) {
+            let context = this._contexts[contextIndex]
+            if (context.callback = )
+        }
+    }
+    emit(eventName, payload) {
+
+    }
 
 }
