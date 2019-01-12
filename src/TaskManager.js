fb9c080dc0b4875453a29b544d946a57fcad820a
Jay <jwinstead@launchdarkly.com>
2019-01-11 18:15:23 -0800

    squash! Automated Development Deployment

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -18,7 +18,6 @@ module.exports = class TaskManager {
 
   request(taskName, payload, responseHandler, responseContext) {
     return new Process(async (process) => {
-      console.log(taskName, 'requested')
       let taskData = {
         closed: false,
         payload, responseHandler, responseContext
@@ -27,7 +26,6 @@ module.exports = class TaskManager {
       this._getTaskList(taskName).push(taskData)
 
       await process.promiseToClose
-      console.log(taskName, 'closed')
       taskData.closed = true
     })
   }
@@ -43,31 +41,21 @@ module.exports = class TaskManager {
       while(process.active) {
         let consumeProcess = this._consume(taskName, subscriptionHandler, context, process)
         await consumeProcess.promiseToClose
-        console.log('subscription ready', taskName, process.closed)
       }
-      console.log('subscription end ', taskName, process.closed)
     })
   }
 
   _consume(taskName, taskHandler, taskContext, parentProcess) {
     return new Process(async (process) => {
-      console.log('consume waiting for task', taskName, process.closed)
       let {payload, responseHandler, responseContext} = await this._getTaskList(taskName).shift()
-      console.log('consume got a task', taskName, process.closed, payload, responseHandler, responseContext)
       if (process.closed) return
-      console.log('consume doing task', taskName, process.closed)
       let taskResult = await taskHandler.call(taskContext, payload)
-      console.log('consume task complete', taskName, process.closed)
       if (process.closed) return
 
       if (responseHandler) {
-        console.log('consume doing response handler', taskName, process.closed)
         responseHandler.call(responseContext, taskResult)
-        console.log('response handler complete', taskName, process.closed)
       } else {
-        console.log('consume no_response_handler', taskName, process.closed)
       }
-      console.log('consume done', taskName, process.closed)
     }, undefined, parentProcess)
   }
 
