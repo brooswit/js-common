a6bbc2d43f4737f29e0fbb8d3310d96745511b3f
Jay <jwinstead@launchdarkly.com>
2019-01-25 15:24:29 -0800

    squash! Automated Development Deployment

--- src/VirtualWebSocket.js
+++ src/VirtualWebSocket.js
@@ -1,7 +1,12 @@
-import { Observable } from 'rxjs';
+import { fromEvent } from 'rxjs';
 
-module.exports = class VirtualWebSocket {
-    constructor(ws) {
+module.exports = class VirtualWebSocket extends process {
+    constructor(ws, optionalParentProcess) {
+        super(optionalParentProcess)
         this._ws = ws;
+        this._observable = fromEvent(ws, "message")
+        this._subscription = this._observable.subscribe(() => {
+
+        })
     }
 }
