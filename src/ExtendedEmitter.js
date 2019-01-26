fffb58a0f90bf1cf1d3a90deacc24e164f31aee6
Jay <jwinstead@launchdarkly.com>
2019-01-25 16:03:07 -0800

    squash! Automated Development Deployment

--- src/ExtendedEmitter.js
+++ src/ExtendedEmitter.js
@@ -4,15 +4,5 @@ const promiseToEmit = require('./promiseToEmit')
 module.exports = class ExtendedEmitter extends EventEmitter {
   async promiseTo(resolveEventName, rejectEventName) {
       return promiseToEmit(this, resolveEventName, rejectEventName)
-    return new Promise((resolve, reject) => {
-        this.on(rejectEventName, resolve)
-        this.on(rejectEventName, resolve)
-    }
-  }
-  close() {
-    if (this.closed) return
-    this.active = false
-    this.closed = true
-    this.promiseToClose.resolve()
   }
 }
