9d6541b2ab1361c69c2fc88fd8b39d14ce170b4e
Jay <jwinstead@launchdarkly.com>
2019-01-25 16:31:45 -0800

    squash! Automated Development Deployment

--- src/Process.js
+++ src/Process.js
@@ -3,17 +3,18 @@ const ExtendedEvents = require('./ExtendedEvents')
 
 module.exports = class Process extends ExtendedEvents {
   constructor(methods, optionalParentProcess) {
-    this._init = methods.init || NO_OP
+    this._init = methods.init
     this._main = methods.main || NO_OP
     this._close = methods.close || NO_OP
     this._closed = false
 
     setTimeout(async () => {
-      await this._init()
+      await (methods.init || NO_OP)()
 
       let promises = []
       promises.push(this.promiseTo('close'))
-      promises.push(method(this))
+      
+      methods.main && promises.push(methods.main(this))
       optionalParentProcess && promises.push(optionalParentProcess.promiseTo('close'))
       await Promise.race(promises)
 
