02a7a3b559fc1f24f181e133c1ffa58acfa86877
Jay <jwinstead@launchdarkly.com>
2019-01-25 17:04:57 -0800

    squash! Automated Development Deployment

--- src/VirtualWebSocket.js
+++ src/VirtualWebSocket.js
@@ -6,8 +6,10 @@ const nextVirtualWebSocketId = 0
 module.exports = class VirtualWebSocket extends Process {
     constructor(ws) {
         super(async () => {
-            this._id = nextVirtualWebSocketId++
             this._ws = ws;
+
+            this._id = nextVirtualWebSocketId++
+            
             this._observable = fromEvent(ws, "message")
             this._subscription = this._observable.subscribe(this._handleMessage)
 
