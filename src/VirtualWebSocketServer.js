e639d782a9fb352dd09296ace809cada5da7b6fd
Jay <jwinstead@launchdarkly.com>
2019-01-26 08:21:41 -0800

    squash! Automated Development Deployment

--- src/VirtualWebSocketServer.js
+++ src/VirtualWebSocketServer.js
@@ -15,34 +15,4 @@ module.exports = class VirtualWebSocketServer extends Process {
     _handleConnection(ws) {
         new VirtualWebSocket(ws, {serverMode: true, parent: this})
     }
-
-    ping() {
-        const pingId = VirtualWebSocket._nextPingId ++
-        this.send('ping', {pingId})
-    }
-    
-    send(event, optionalPayload, optionalChannel) {
-        const channel = optionalChannel || this._channel
-        const payload = optionalPayload
-        this._ws.send({ channel, event, payload })
-    }
-
-    _handleMessage(rawMsg) {
-        const msg = JSONparseSafe(rawMsg)
-        const {channel, event, payload} = msg.event
-        if (channel === this._channel) {
-            if (event === 'close') {
-                this.close()
-            } else if (event === 'message') {
-                this.emit('message', payload)
-            } else if (event === 'ping') {
-                this.send('pong', payload)
-                this.emit('ping', payload)
-            }
-        }
-    }
 }
-
-VirtualWebSocket._nextVirtualWebSocketChannel = 0
-VirtualWebSocket._nextPingId = 0
-VirtualWebSocket._nextMessageId = 0
