917f690dc9ab0a9c12876a5b35574f84d529c5ae
Jay <jwinstead@launchdarkly.com>
2019-01-29 06:55:52 -0800

    squash! Automated Development Deployment

--- src/Process.js
+++ src/Process.js
@@ -25,7 +25,7 @@ module.exports = class Process extends ExtendedEvents {
   subscribeTo(observable, handler) {
     if (this.isActive()) {
       const subscription = observable.subscribe(handler)
-      new Process((process) => {
+      new Process(async (process) => {
         await process.untilEnd
         subscription.unsubscribe()
       }, this)
