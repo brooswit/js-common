4a97ce10c209010dafdd3c759a060e65b4beea70
Jay <jwinstead@launchdarkly.com>
2019-01-31 19:05:23 -0800

    squash! Automated Development Deployment

---
+++ src/classes/Church.js
@@ -0,0 +1,24 @@
+module.exports = class Church {
+    constructor(namespace) {
+        this._parent = 
+        this._namespace = namespace
+    }
+    error() {
+
+    }
+    warn() {
+
+    }
+    info() {
+
+    }
+    verbose() {
+
+    }
+    debug() {
+
+    }
+    silly() {
+
+    }
+}
