6ad1c39a0336a4c9e5250891bd33d44f3fc1271a
Jay <jwinstead@launchdarkly.com>
2019-01-31 19:11:21 -0800

    squash! Automated Development Deployment

--- src/classes/Church.js
+++ src/classes/Church.js
@@ -14,21 +14,33 @@ module.exports = class Church {
     }
 
     error() {
-        winston.log()
+        let args = Array.prototype.slice.call(arguments)
+        args.unshift('error')
+        winston.log.apply(winston, args)
     }
     warn() {
-
+        let args = Array.prototype.slice.call(arguments)
+        args.unshift('warn')
+        winston.log.apply(winston, args)
     }
     info() {
-
+        let args = Array.prototype.slice.call(arguments)
+        args.unshift('info')
+        winston.log.apply(winston, args)
     }
     verbose() {
-
+        let args = Array.prototype.slice.call(arguments)
+        args.unshift('verbose')
+        winston.log.apply(winston, args)
     }
     debug() {
-
+        let args = Array.prototype.slice.call(arguments)
+        args.unshift('debug')
+        winston.log.apply(winston, args)
     }
     silly() {
-
+        let args = Array.prototype.slice.call(arguments)
+        args.unshift('silly')
+        winston.log.apply(winston, args)
     }
 }
