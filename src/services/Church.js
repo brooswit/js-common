dbce851e7448009e0b9b8a57f66ac80aa17dbdf5
Jay <jwinstead@launchdarkly.com>
2019-01-31 20:00:28 -0800

    squash! Automated Development Deployment

--- src/services/Church.js
+++ src/services/Church.js
@@ -9,8 +9,7 @@ const logger = winston.createLogger({
 
 class Church {
     constructor(namespace) {
-        console.log(namespace)
-        this._namespace = namespace
+        this._namespace = namespace || "-"
     }
 
     create(namespace) {
