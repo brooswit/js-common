d98fb50bdeebd5493a9dbc55ba94fef380e10de4
Jay <jwinstead@launchdarkly.com>
2019-01-31 19:49:03 -0800

    squash! Automated Development Deployment

--- src/classes/Job.js
+++ src/classes/Job.js
@@ -4,54 +4,53 @@ const chrono = require('../services/chrono')
 const church = require('../services/church')
 
 module.exports = class Job extends ExtendedEmitter {
-  constructor(mainHandler, optionalParent) {
-    super()
-      console.log('jobby')
-      run(async () => {
-      console.log('new job')
-      this._active = true
+    constructor(mainHandler, optionalParent) {
+        super()
+            run(async () => {
+            console.log('new job')
+            this._active = true
 
-      this._promiseToEnd = this.promiseTo('end')
+            this._promiseToEnd = this.promiseTo('end')
 
-      const promiseParentWillClose = optionalParent && optionalParent.untilEnd
-      const promiseThisWillEnd = this.untilEnd
-      const promiseThisWillComplete = async () => {
-        await chrono.delay()
-        return await mainHandler(this)
-      }
+            const promiseParentWillClose = optionalParent && optionalParent.untilEnd
+            const promiseThisWillEnd = this.untilEnd
+            const promiseThisWillComplete = async () => {
+                await chrono.delay()
+                return await mainHandler(this)
+            }
 
-      const allPromises = [promiseThisWillEnd, promiseThisWillComplete]
-      if (optionalParent) { allPromises.push(promiseParentWillClose) }
+            const allPromises = [promiseThisWillEnd, promiseThisWillComplete]
+            if (optionalParent) { allPromises.push(promiseParentWillClose) }
 
-      await Promise.race(allPromises)
+            await Promise.race(allPromises)
 
-      this.end()
-    })
-  }
-
-  get untilEnd() {
-    return this._promiseToEnd
-  }
-
-  get isActive() {
-    return !!this._active
-  }
-
-  subscribeTo(observable, handler) {
-    if (this.isActive) {
-      const subscription = observable.subscribe(handler)
-      new Job(async (job) => {
-        await job.untilEnd
-        subscription.unsubscribe()
-      }, this)
-      return subscription
+            this.end()
+        })
     }
-  }
 
-  end() {
-    if (!this.isActive) return false
-    this._active = false
-    this.emit('end')
-    return true
-  }
+    get untilEnd() {
+        return this._promiseToEnd
+    }
+
+    get isActive() {
+        return !!this._active
+    }
+
+    subscribeTo(observable, handler) {
+        if (this.isActive) {
+        const subscription = observable.subscribe(handler)
+        new Job(async (job) => {
+            await job.untilEnd
+            subscription.unsubscribe()
+        }, this)
+        return subscription
+        }
+    }
+
+    end() {
+        if (!this.isActive) return false
+        this._active = false
+        this.emit('end')
+        return true
+    }
 }
