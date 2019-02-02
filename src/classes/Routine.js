3305de5885a87ef5a7e1032f497ed22359b2c9a4
Jay <jwinstead@launchdarkly.com>
2019-02-01 18:02:16 -0800

    squash! Automated Development Deployment

--- src/classes/Routine.js
+++ src/classes/Routine.js
@@ -7,9 +7,8 @@ module.exports = class Routine extends ExtendedEmitter {
     constructor(mainHandler, optionalParent) {
         super()
         run(async () => {
-            this._logger = namespace(this.constructor.name)
-            this.log = this._logger.log
-            this.log('warn', 'test')
+            this.log = namespace(this.constructor.name)
+            this.log.warn('test')
             this._active = true
 
             this._promiseToEnd = this.promiseTo('end')
