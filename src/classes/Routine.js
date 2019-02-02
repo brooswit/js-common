c6d0353aff7005673ccdefa04f342b67aea5c765
Jay <jwinstead@launchdarkly.com>
2019-02-01 20:12:51 -0800

    squash! Automated Development Deployment

--- src/classes/Routine.js
+++ src/classes/Routine.js
@@ -40,6 +40,7 @@ module.exports = class Routine extends ExtendedEmitter {
     }
 
     get isActive() {
+        this.log.info(this._active)
         return !!this._active
     }
 
@@ -56,7 +57,6 @@ module.exports = class Routine extends ExtendedEmitter {
 
     end() {
         if (!this.isActive) return false
-        this.log.info('end')
         this._active = false
         this.emit('end')
         return true
