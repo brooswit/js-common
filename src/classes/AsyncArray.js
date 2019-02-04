389373c61a01b9242a9000a748da163437b33fb1
Jay <jwinstead@launchdarkly.com>
2019-02-04 07:54:35 -0800

    squash! Automated Development Deployment

--- src/classes/AsyncArray.js
+++ src/classes/AsyncArray.js
@@ -50,8 +50,8 @@ class AsyncArray extends Routine {
   }
 
   _processQueues() {
-    this.log.info('processing')
-    while(this._requestQueue.length > 0 && (!this.isActive || this._payloadQueue > 0) ) {
+    this.log.info('processing', this.isActive, )
+    while(this._requestQueue.length > 0 && (!this.isActive || this._payloadQueue.length > 0) ) {
       const {action, resolver} = this._requestQueue.shift()
       this.log.info('processing ' + action)
       resolver.resolve(this._payloadQueue[action]())
