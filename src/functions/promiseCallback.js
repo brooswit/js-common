0ff363d5d4aeaca2e899615284faa6f0231ce8d0
Jay <jwinstead@launchdarkly.com>
2019-02-10 19:17:26 -0800

    squash! Automated Development Deployment

--- src/functions/promiseCallback.js
+++ src/functions/promiseCallback.js
@@ -2,8 +2,7 @@ const Resolver = require('../classes/Resolver')
 
 module.exports = async function promiseCallback(context, method) {
     const resolver = new Resolver()
-    console.log(arguments)
-    const args = Array.slice.call(arguments, 2)
+    const args = Array.prototype.slice.call(arguments, 2)
     args.push(resolver.resolve)
     method.call(context, args)
     return await resolver
