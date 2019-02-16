7c6c463bd9c2684b84caf17f621ff99bfd463a7a
Jay <jwinstead@launchdarkly.com>
2019-02-15 19:27:48 -0800

    squash! Automated Development Deployment

--- src/classes/TaskManager.js
+++ src/classes/TaskManager.js
@@ -35,9 +35,9 @@ module.exports = class TaskManager extends Routine {
   }
 
   async consume(taskName) {
+    if (!this.isActive) return undefined
     const future = new Future()
-    if (!this.isActive) return null
-    const consumePromise = this._consume(taskName, future)
+    this._consume(taskName, future)
     if (optionalHandler) {
       consumePromise.then(optionalHandler)
     } else {
