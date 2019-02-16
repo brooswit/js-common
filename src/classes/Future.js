d34dee7126efbc733bd450e584904338f59e0ecd
Jay <jwinstead@launchdarkly.com>
2019-02-15 18:57:17 -0800

    squash! Automated Development Deployment

--- src/classes/Future.js
+++ src/classes/Future.js
@@ -1,10 +1,13 @@
 module.exports = class Future extends ExtendedEmitter {
     constructor() {
         this.untilSet = this.promiseTo('set')
+        this.on('set', ()=>{
+            this.untilSet = this.promiseTo('set')
+        })
     }
 
     async get() {
-        await this.promise
+        await untilSet
     }
 
     set(value) {
