995a21bcab18629fa30bdbdc9980cac77f16dd76
Jay <jwinstead@launchdarkly.com>
2019-02-15 16:08:42 -0800

    squash! Automated Development Deployment

--- src/classes/AsyncArray.js
+++ src/classes/AsyncArray.js
@@ -3,6 +3,7 @@ const Routine = require('../classes/Routine')
 
 class AsyncArray extends Routine {
   constructor () {
+    this.isStopped = false
     this._requestQueue = []
     this._elementQueue = []
   }
@@ -23,6 +24,10 @@ class AsyncArray extends Routine {
     return await this._get('shift')
   }
 
+  stop() {
+    this.isStopped = true
+  }
+
   _put (action, element) {
     if(!this.isActive) return false
     this._elementQueue[action](element)
@@ -40,7 +45,7 @@ class AsyncArray extends Routine {
   }
 
   _processQueues() {
-    while(this._requestQueue.length > 0 && (!this.isActive || this._elementQueue.length > 0) ) {
+    while(this._requestQueue.length > 0 && (this.isStopped || this._elementQueue.length > 0) ) {
       const {action, resolver} = this._requestQueue.shift()
       resolver.resolve(this._elementQueue[action]())
     }
