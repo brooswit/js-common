aa496cb012f2b4c2cf4cde567bef45309c00141a
Jay <jwinstead@launchdarkly.com>
2019-02-17 08:45:58 -0800

    squash! Automated Development Deployment

--- src/classes/TaskManager.js
+++ src/classes/TaskManager.js
@@ -47,7 +47,7 @@ module.exports = class TaskManager extends Routine {
     return new Routine(async (routine) => {
       while(routine.isActive) {
         const task = await this.consume(taskName)
-        task.set(await subscriptionHandler(task))
+        await subscriptionHandler(task)
         await task.get()
       }
     }, this, `${taskName} Subscription`)
