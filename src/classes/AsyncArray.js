cf0c987724f7377cac8c14702c99fed122c6ca0b
Jay <jwinstead@launchdarkly.com>
2019-02-17 01:01:27 -0800

    squash! Automated Development Deployment

--- src/classes/AsyncArray.js
+++ src/classes/AsyncArray.js
@@ -44,7 +44,7 @@ class AsyncArray {
   _processQueues() {
     while(this._requestQueue.length > 0 && (this.isStopped || this._elementQueue.length > 0) ) {
       const {action, resolver} = this._requestQueue.shift()
-      element = this._elementQueue[action]()
+      const element = this._elementQueue[action]()
       resolver.resolve(element)
     }
   }
