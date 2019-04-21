ce29a1186202781615414d5126c1cbbbfbae73ab
brooswit <brooswit@OldBoi.local>
2019-04-21 09:51:28 -0700

    squash! Automated Development Deployment

--- src/functions/extendWs.js
+++ src/functions/extendWs.js
@@ -56,6 +56,12 @@ module.exports = function extendWs(ws, enableDebug) {
     ws.once = ws._emitter.once.bind(ws._emitter)
     ws.emit = ws._emitter.emit.bind(ws._emitter)
 
+    thing = function() {
+      let args = Array.prototype.slice.call(arguments)
+      args.unshift('open')
+      this.emit.apply(this, args)
+    }
+    
     ws.onopen = function() {
       let args = Array.prototype.slice.call(arguments)
       args.unshift('open')
